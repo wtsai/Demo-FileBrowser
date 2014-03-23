b478e7201b0816f3f250d27a0e36ea24b0b01668
WesleyTsai <wesleyboy42@gmail.com>
2014-03-23 15:11:02 +0800

    Change DirReader function by layer of folder.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ if (!process.env.NODE_ENV)
 	process.env.NODE_ENV = 'development';
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 9011);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -44,6 +44,6 @@ var courser = new Courser(app);
 courser.addPath(__dirname + '/routes');
 courser.init(function() {
 		http.createServer(app).listen(app.get('port'), function(){
-		console.log('Express server listening on port ' + app.get('port'));
+		console.log('FileBrowser server listening on port ' + app.get('port'));
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
-//var fs = require('fs');
-var path = require('path')
+var path = require('path');
 var DirReader = require( 'DirReader' );
+var ROOTFOLDER = './public/share';
+var ASSIGNEDPATH = '/';
 
 module.exports = {
 	'/': index,
 	'/user': user,
-	'/File/Dir':  {
-		get: readdir,
-		post: updatedir
-	},
+	'/File/Dir/*': readdir,
+	'/File/Dir': readdir
 }
 
 function index (req, res) {
@@ -20,47 +19,37 @@ function user (req, res) {
 };
 
 function updatedir(req, res) {
-
 	res.end('POST Method');
 };
 
 function readdir (req, res) {
 	var DirList =  {};
 	var DirListIndex = 0;
-	console.log( req.method );
-	//console.log( DirList[DirListIndex] );
-	//console.log( DirList[DirListIndex]['type'] );
-	DirReader.RetrieveDirSync( './public/share', function( err, DirPath, stats, ifDir ){
-	//DirReader.RetrieveDir( './public/share', function( err, DirPath, stats, ifDir ){
-	//DirReader.ReadDir( './public/share', function( err, DirPath, stats, ifDir ){
+
+    if (req.params.ROOT)
+    {
+        ROOTFOLDER = './' + req.params.ROOT;
+    }
+    
+    if (req.params[0])
+    {
+        ASSIGNEDPATH = '/' + req.params[0];
+    } 
+	DirReader.ReadDir( ROOTFOLDER,  ASSIGNEDPATH, function( err, DirPath, stats, ifDir ){
 
 		if( err ){
 			console.log( '[ ERROR ]', err );
 		}
 		else {
-			//DirList[DirListIndex]['type'] = ( ifDir ? 'DIRECTORY' : 'FILE' );
-			//console.log( '[ ifDir ]', ifDir );
-			//console.log( '[ DirPath ]', DirPath );
 			DirList[DirListIndex] = {
 				"type": ( ifDir ? 'Directory' : 'File' ),
 				"path": path.normalize(DirPath)
 			};
-			/* set all the properties with anonymous function
-			DirList = new function () {
-				this[DirListIndex] = {
-					"type": ifDir,
-					"path": DirPath
-				};
-			};
-			*/
-			//console.log( '[' + DirList[DirListIndex]['type'] + '] ' + DirList[DirListIndex]['path'] + '\n');
+
 			DirListIndex += 1;
 		}
 	}, function(){
-		console.log( DirList );
 		res.send(DirList);
 	});
 
-	//console.log( DirList );
-	//res.send("The directory reading is not finished!");
 };
